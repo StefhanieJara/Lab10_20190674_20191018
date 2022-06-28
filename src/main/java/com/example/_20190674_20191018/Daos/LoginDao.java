1ad179a111d260ac76646694951061e98e2d70cf
nipanaque <a20191018@pucp.edu.pe>
2022-06-28 00:44:35 -0500

    Daos//Servlet

--- src/main/java/com/example/_20190674_20191018/Daos/LoginDao.java
+++ src/main/java/com/example/_20190674_20191018/Daos/LoginDao.java
@@ -1,4 +1,41 @@
 package com.example._20190674_20191018.Daos;
 
+import com.example._20190674_20191018.Beans.Usuario;
+
+import java.sql.*;
+
 public class LoginDao {
+    public Usuario validar(String correo, String password){
+        Usuario usuario = null;
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/lab10";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String sql = "SELECT * FROM usuario WHERE correoPucp =? AND contrasenha=sha2(?,256)";
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             PreparedStatement pstmt = connection.prepareStatement(sql);) {
+
+            pstmt.setString(1,correo);
+            pstmt.setString(2,password);
+            try(ResultSet rs = pstmt.executeQuery()){
+                if(rs.next()){
+                    usuario = new Usuario();
+                    usuario.setCodigoPUCP(rs.getInt(1));
+                    usuario.setCorreoPucp(rs.getString(5));
+                    usuario.setEspecialidad(rs.getString(6));
+                }
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usuario;
+    }
 }
--- src/main/java/com/example/_20190674_20191018/Servlets/LoginServlet.java
+++ src/main/java/com/example/_20190674_20191018/Servlets/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.example._20190674_20191018.Servlets;
 
+import com.example._20190674_20191018.Beans.Usuario;
+import com.example._20190674_20191018.Daos.LoginDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,11 +12,46 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher;
+        String logout = request.getParameter("finish");
+        if(logout == null){
+            requestDispatcher = request.getRequestDispatcher("index.jsp");
+            requestDispatcher.forward(request,response);
+        }else{
+            if(logout.equals("yes")){
+                HttpSession session = request.getSession();
+                session.invalidate();
+                response.sendRedirect(request.getContextPath() + "/LoginServlet");
+            }else{
 
+                requestDispatcher = request.getRequestDispatcher("index.jsp");
+                requestDispatcher.forward(request,response);
+
+            }
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LoginDao loginDao = new LoginDao();
+        String correo = request.getParameter("correo");
+        String pass = request.getParameter("pass");
+        Usuario usuario = loginDao.validar(correo, pass);
+        HttpSession session = request.getSession();
 
+        if(usuario !=null){
+
+            session.setAttribute("usuarioSesion",usuario);
+            session.setAttribute("especialidad",usuario.getEspecialidad());
+            session.setMaxInactiveInterval(60*60);
+            if(usuario.getEspecialidad() == "Telecomunicaciones"){
+                response.sendRedirect(request.getContextPath()+"/MenuServlet");
+            }else{
+                response.sendRedirect(request.getContextPath()+"/LoginServlet");
+            }
+        }else{
+            session.setAttribute("indicador","error");
+            response.sendRedirect(request.getContextPath()+"/LoginServlet");
+        }
     }
 }
